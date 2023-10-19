c8326eab343428f6c0be65592c986284d4b1c838
jeyanthiran97 <jeyanthiran97@gmail.com>
2023-10-20 00:29:45 +0530

    MUI NavNar Created & login component insert

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,17 @@ import Head from 'next/head'
 
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Header from '@/components/Header'
+import SideMenu from '@/components/SideMenu'
 import Dashboard from "@/pages/dashboard/Dashboard"
-import Header from './components/Header'
-import SideMenu from './components/SideMenu'
+import {useSession} from "next-auth/react"
+import Login from './login/Login'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <>
       <Head>
@@ -17,11 +21,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${inter.className}`}>
         <div>
           <Header />
-          <SideMenu />
-          <Dashboard />
+          {
+            session && (
+              <div className={`${styles.main} `}>
+                <SideMenu />
+                <Dashboard />
+              </div>
+            )
+          }
+          <Login />
         </div>
       </main>
     </>
