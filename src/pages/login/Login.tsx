43a64e343cd19fd525af25a32f3830e993121dbb
jeyanthiran97 <jeyanthiran97@gmail.com>
2023-10-20 00:12:28 +0530

    Authentication with NextAuthJS with google cloud

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useSession, signIn, signOut } from "next-auth/react"
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import styles from '@/styles/Home.module.css'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  const { data: session} = useSession();
+  const userProfileImg = session?.user?.image as string;
+
+  if (session) {
+    return <div className={`${styles.main} `}>
+      <h4><u>Sign In User Details</u></h4>
+      <h3>Name : {session?.user?.name} </h3>
+      <p>E-mail : {session?.user?.email}</p>
+      <Avatar alt={session?.user?.name as string} src={userProfileImg} />
+      <Button variant="contained" color="error" onClick={() => signOut()}>Sign out</Button>
+    </div>
+  }
+
+  return <div className={`${styles.main} `}>
+    Not Signed in <br/>
+    <Button variant="contained" color="success" onClick={() => signIn()}>Sign in</Button>
+  </div>
 }
 
 export default Login
