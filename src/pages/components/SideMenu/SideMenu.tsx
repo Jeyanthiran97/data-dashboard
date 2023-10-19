d7436da3f9c3195e7c821c1ba37cb61679dde9a4
jeyanthiran97 <jeyanthiran97@gmail.com>
2023-10-19 17:02:51 +0530

    Basic route created (project setup)

---
+++ src/pages/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideMenu = () => {
+  return (
+    <ul>
+      <li>Data</li>
+      <li>Profile</li>
+      <li>Setting</li>
+    </ul>
+  )
+}
+
+export default SideMenu
