8756dfa787d1b40d55c8dbd56649603ca26721b2
jeyanthiran97 <jeyanthiran97@gmail.com>
2023-10-20 16:20:49 +0530

    Layout theming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,35 @@
 import Head from 'next/head'
-import Header from '@/components/Header'
-import SideMenu from '@/components/SideMenu'
-import Dashboard from "@/pages/dashboard/Dashboard"
-import {useSession} from "next-auth/react"
-import Login from './login/Login'
+import Dashboard from "@/pages/dashboard";
+import SideMenu from "@/components/SideMenu";
+// import Login from "@/components/Login";
+import {useSession} from "next-auth/react";
+import scss from './Home.module.scss';
+import React from "react";
 
-export default function Home() {
-  const { data: session } = useSession();
+const Home: React.FC = () => {
+    const { data: session } = useSession();
 
-  return (
-    <>
-      <Head>
-        <title>Data Dashboard</title>
-        <meta name="description" content="Data Dashboard" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <div>
-          <Header />
-          {
-            session && (
-              <div>
-                <SideMenu />
-                <Dashboard />
-              </div>
-            )
-          }
-        </div>
-      </main>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>DataSoft - Dashboard</title>
+                <meta name="description" content="Data Dashboard" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={scss.main}>
+                {
+                    session && (
+                    <>
+                        <SideMenu />
+                        <Dashboard />
+                    </>
+                    )
+                }
+                {/* <Login /> */}
+            </main>
+        </>
+    )
 }
+
+export default Home;
---
+++ src/theme/lightTheme.ts
@@ -0,0 +1,18 @@
+import {ThemeOptions} from "@mui/material";
+
+const lightTheme: ThemeOptions = {
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#fffff0',
+        },
+        secondary: {
+            main: '#000000',
+        },
+        background: {
+            default: '#E5E8E8'
+        }
+    }
+}
+
+export default lightTheme;
